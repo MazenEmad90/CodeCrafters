e0b57650a942d6026f939ad9a2f9345993025ef9
MAjjood48 <majjood717@gmail.com>
2026-03-05 02:15:33 -0800

    Update main.dart

--- Code/lib/Pomodoro+/main.dart
+++ Code/lib/Pomodoro+/main.dart
@@ -1,2 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'providers/focus_provider.dart';
+import 'screens/focus_screen.dart';
 
+void main() {
+  runApp(const FocusApp());
+}
+
+class FocusApp extends StatelessWidget {
+  const FocusApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => FocusProvider(),
+      child: MaterialApp(
+        title: 'Focus Timer',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness:              Brightness.dark,
+          scaffoldBackgroundColor: const Color(0xFF0A1F0F),
+          fontFamily:              'Roboto',
+        ),
+        home: const FocusScreen(),
+      ),
+    );
+  }
+}
